323c3717f3b7e8aeb6bf68ad3a63a52be82d2209
akann0 <akann@ucsd.edu>
2022-05-01 21:15:11 -0700

    Changes made

--- demofile.java
+++ demofile.java
@@ -1,6 +1,6 @@
 public class demofile{
     static String seven() {
-        return Integer.toString(6);
+        return Integer.toString(7);
     }
     public static void main(String[] args){
         System.out.print(seven());
