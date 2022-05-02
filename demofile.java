aac97d396e807a3f66226449bc113eb7cc69fd7a
akann0 <akann@ucsd.edu>
2022-05-01 21:08:35 -0700

    Skills Assessment created

---
+++ demofile.java
@@ -0,0 +1,8 @@
+public class demofile{
+    static String seven() {
+        return Integer.toString(6);
+    }
+    public static void main(String[] args){
+        System.out.print(seven());
+    }
+}
---
+++ demotest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class demotest {
+    @Test 
+    public test() {
+        assertEquals("7", seven());
+    }
+}
